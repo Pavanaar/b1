d5a27287439e45a7a68a340441c5dc327b3d10ca
pavanaar <pavanagowda2018@gmail.com>
2024-07-10 12:38:12 +0530

    2nd try

--- test/specs/assertion/assertions.js
+++ test/specs/assertion/assertions.js
@@ -1,14 +1,18 @@
+const { expect } = require("chai")
+
 //browsermatches
 describe("",async()=>{
     it("",async()=>{
         await browser.url("https://www.flipkart.com/")
         //await browser.$('//input[@class="Pke_EE"]').setValue("mobiles")
-        await expect(browser).toHaveUrl("https://www.flipkart.com/")
+        let url=await browser.getUrl()
+         expect(url). to.be.equal("https://www.flipkart.com/")
+        //await expect(browser).toHaveUrl("https://www.flipkart.com/")
         
         let ele=await browser.$('//input[@class="Pke_EE"]')
-        ele.waitForEnabled()
+        //ele.waitForEnabled()
             
-        await expect(ele).toBeEnabled()
+        //await expect(ele).toBeEnabled()
 
 
 
